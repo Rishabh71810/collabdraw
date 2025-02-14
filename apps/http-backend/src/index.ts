2fbca1ee117d733d5ca8ab77d1ac8aa8a5f989cd
Rishabh <rishabh19772004@gmail.com>
2025-02-14 17:46:35 +0530

    refactor: restructure backend configuration and validation schemas

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,14 +1,28 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from './config';
+import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from './middleware';
+import {CreateUserSchema ,SigninsSchema,CreateRoomSchema} from "@repo/common/types"
 const app = express();
 
 app.post("/signup", (req, res) => {
-
+const data = CreateUserSchema.safeParse(req.body);
+if(!data.success){
+   res.json({
+    "message": "Incorrect inputs"
+  })
+  return;
+}
 })
 
 app.post("/signin", (req, res) => {
+  const data = SigninsSchema.safeParse(req.body);
+if(!data.success){
+   res.json({
+    "message": "Incorrect inputs"
+  })
+  return;
+}
   const userId=1;
   const token = jwt.sign({
 
@@ -20,7 +34,13 @@ app.post("/signin", (req, res) => {
 })
 
 app.post("/room",middleware, (req, res) => {
-
+  const data = CreateRoomSchema.safeParse(req.body);
+  if(!data.success){
+     res.json({
+      "message": "Incorrect inputs"
+    })
+    return;
+  }
 })  
 
 app.listen(3000);
