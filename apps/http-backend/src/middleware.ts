d27ba7d75be11b49e4c1edc801c55a237108533b
Rishabh <rishabh19772004@gmail.com>
2025-02-14 16:17:53 +0530

    implement JWT authentication and setup Express server for HTTP and WebSocket backends

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
+
+// Extend Request type to include userId becsause typescript doesn't recognise userId as a property of Request
+interface AuthenticatedRequest extends Request {
+  userId?: string;
+}
+
+export function middleware(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const token = req.headers["authorization"] ?? "";
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+}
