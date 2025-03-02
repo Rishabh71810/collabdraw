87fcc119ce9268a50aefa1729495603211da5e8a
Rishabh <rishabh19772004@gmail.com>
2025-03-03 02:29:17 +0530

    feat: enhance signup endpoint with async handling and error responses

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -5,14 +5,35 @@ import { middleware } from './middleware';
 import {CreateUserSchema ,SigninsSchema,CreateRoomSchema} from "@repo/common/types"
 import {prismaClient} from '@repo/db/client'
 const app = express();
+app.use(express.json());
 
-app.post("/signup", (req, res) => {
-const data = CreateUserSchema.safeParse(req.body);
-if(!data.success){
+app.post("/signup", async (req, res) => {
+const parsedData = CreateUserSchema.safeParse(req.body); // used safe parse that suceess does not return any error 
+if(!parsedData.success){
    res.json({
     "message": "Incorrect inputs"
   })
   return;
+}
+try {
+  await prismaClient.user.create({
+    data: {
+      email:parsedData.data?.username,
+      password:parsedData.data.password,
+      name:parsedData.data.name,
+      photo:""
+    }
+  })
+  res.json({
+    userId: "123"
+  })
+
+} catch (error) {
+  res.status(411).json({
+    "message": "User already exists"
+  })
+  return;
+  
 }
 })
 
